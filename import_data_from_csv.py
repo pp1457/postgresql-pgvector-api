4365fc685c7168ebf36fb5c29dc87b40294f44b9
Paul Liao <liaoyunyang0815@gmail.com>
2024-08-22 15:28:17 +0800

    update code

--- import_data_from_csv.py
+++ import_data_from_csv.py
@@ -1,19 +1,23 @@
 import csv
 import ast
 import json
+from insert_chunk import insert_chunk
 
 def read_csv_to_dict_list(file_path):
-    with open(file_path, mode='r', encoding='utf-8-sig') as file:
+    """ read csv to dict list """
+
+    with open(file_path, mode='r', encoding='utf-8') as file:
         csv_reader = csv.DictReader(file)
         dict_list = [row for row in csv_reader]
     return dict_list
 
 def main():
+    """ main """
     file_path = input("File Path: ")
     table = input("Table Name: ")
     chunks = read_csv_to_dict_list(file_path)
 
-    for chunk in data:
+    for chunk in chunks:
         chunk["chunk_id"] = int(chunk["chunk_id"])
         chunk["line_range"] = ast.literal_eval(chunk["line_range"])
         chunk["page_range"] = ast.literal_eval(chunk["page_range"])
@@ -22,4 +26,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
